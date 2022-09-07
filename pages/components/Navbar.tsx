c6f530d108ade4b6e12397ce7f6c7582f3cdf5b3
Mr.chu <chuhongkyu@gmail.com>
2022-09-07 20:22:08 +0900

    nav 바

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
 import css from "styled-jsx/css";
 
 const style = css`
@@ -5,12 +7,48 @@ const style = css`
     width: 100%;
     border: 1px solid black;
   }
+  .nav_btn {
+    display: inline-block;
+    padding: 1rem;
+    position: relative;
+  }
+  .active {
+    position: absolute;
+    width: 100%;
+    height: 5px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: 0 auto;
+    z-index: 99;
+    background-color: black;
+    transition: 0.5s;
+  }
 `;
 
 const Navbar = () => {
+  const router = useRouter();
   return (
     <>
-      <nav className="nav">Nav</nav>
+      <nav className="nav">
+        <Link href="/">
+          <a className="nav_btn">
+            Home {router.pathname === "/" ? <div className="active" /> : null}
+          </a>
+        </Link>
+        <Link href="/shop">
+          <a className="nav_btn">
+            Shop{" "}
+            {router.pathname === "/shop" ? <div className="active" /> : null}
+          </a>
+        </Link>
+        <Link href="/login">
+          <a className="nav_btn">
+            Login{" "}
+            {router.pathname === "/login" ? <div className="active" /> : null}
+          </a>
+        </Link>
+      </nav>
       <style jsx>{style}</style>
     </>
   );
---
+++ pages/shop.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Shop: NextPage = () => {
+  return <div>Shop</div>;
+};
+
+export default Shop;
