ab453358befdbd105c117f7f940b30d9535c5a4b
Mr.chu <chuhongkyu@gmail.com>
2022-09-14 20:12:08 +0900

    api interface

--- pages/api/api.ts
+++ pages/api/api.ts
@@ -1,25 +1,25 @@
-const COLLECTION_SLUG = "by-art";
-//현재 사용할 api
-export function getNFT() {
-  return fetch(
-    `https://api.opensea.io/api/v1/collection/${COLLECTION_SLUG}`
-  ).then((response) => response.json());
-}
+// //리액트 쿼리 사용 시
+// export function getNFT() {
+//   return fetch(
+//     `https://api.opensea.io/api/v1/collection/${COLLECTION_SLUG}`
+//   ).then((response) => response.json());
+// }
 
 // //서버 사이드 고정 함수
 // export async function getServerSideProps() {
-//     // const options = { method: "GET" };
-//     const COLLECTION_SLUG = "by-art";
-//     // fetch(`https://api.opensea.io/api/v1/collection/${COLLECTION_SLUG}`, options)
-//     //   .then((response) => response.json())
-//     //   .then((response) => {
-//     //     console.log(response);
-//     //   })
-//     //   .catch((err) => console.error(err));
-//     const { results } = await (
-//       await fetch(`https://api.opensea.io/api/v1/collection/${COLLECTION_SLUG}`)
-//     ).json();
-//     return {
-//       props: { results },
-//     };
-//   }
+//   const { results } = await (
+//     await fetch(`https://api.opensea.io/api/v1/collection/${COLLECTION_SLUG}`)
+//   ).json();
+//   return {
+//     props: { results },
+//   };
+// }
+
+// const options = { method: "GET" };
+
+// fetch(`https://api.opensea.io/api/v1/collection/${COLLECTION_SLUG}`, options)
+//   .then((response) => response.json())
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((err) => console.error(err));
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,53 @@
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
-import { getNFT } from "./api/api";
+import css from "styled-jsx/css";
+
+const style = css`
+  /* .banner {
+    width: 100%;
+    border: 1px solid black;
+    height: 400px;
+  } */
+`;
+
+interface Inft {
+  banner_image_url: string;
+  created_date: string;
+  description: string;
+  featured_image_url: string;
+  image_url: string;
+}
+
+const COLLECTION_SLUG = "by-art";
 
 const Home: NextPage = () => {
-  const [items, setItems] = useState();
+  const [items, setItems] = useState<Inft>();
   //api 호출
   useEffect(() => {
-    getNFT().then((response) => {
-      setItems(response);
-      console.log(items);
-    });
+    (async () => {
+      const { collection } = await (
+        await fetch(
+          `https://api.opensea.io/api/v1/collection/${COLLECTION_SLUG}`
+        )
+      ).json();
+      setItems(collection);
+      console.log(items?.banner_image_url);
+    })();
   }, []);
+
   return (
     <>
       <div>Home</div>
+      <div
+        style={{
+          width: "100%",
+          border: "1px solid black",
+          height: "400px",
+          backgroundImage: items?.banner_image_url,
+        }}
+      ></div>
+      <style jsx>{style}</style>
     </>
   );
 };
